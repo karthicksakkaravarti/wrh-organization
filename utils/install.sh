015f16fab9de004613b0010838543d8f520e52f7
Ali Eghlima <pprolancer@gmail.com>
2022-12-05 20:02:43 +0400

    - updated the install.sh

--- utils/install.sh
+++ utils/install.sh
@@ -8,15 +8,12 @@ fi
 echo "+++ installing required libs ..."
 sudo apt-get update
 # sudo apt-get install postgresql postgresql-contrib postgresql-server-dev-all -y
+sudo apt-get install postgresql-client -y
 sudo apt-get install nginx -y
 sudo apt-get install python3-dev -y
 sudo apt-get install python3-pip -y
 sudo apt-get install git -y
 sudo apt-get install supervisor -y
-sudo apt-get install libjpeg-dev zlib1g-dev -y
-sudo apt-get install libffi-dev -y
-sudo apt-get install libcairo2 -y
-sudo apt-get install pango1.0-tests -y
 sudo apt-get install redis-server -y
 sudo pip install virtualenv
 
